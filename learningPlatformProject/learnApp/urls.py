da1ad830c1b11384b0e5918663dd1af009885df1
mnbvcxz200 <sam.tas.ti.00@gmail.com>
2022-05-24 17:31:07 +0300

    Added settings page, no functionality.

--- learningPlatformProject/learnApp/urls.py
+++ learningPlatformProject/learnApp/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('update_workspace/<str:pk>/',views.updateWorkspace, name="update_workspace"),
     path('delete_workspace/<str:pk>/',views.deleteWorkspace, name="delete_workspace"),
     path('delete_message/<str:pk>/', views.deleteMessage, name="delete_message"),
-    path('profile/<str:pk>/', views.userProfile, name="user_profile")
+    path('profile/<str:pk>/', views.userProfile, name="user_profile"),
+    path('update_user/', views.updateUser, name="update_user"),
 ]
