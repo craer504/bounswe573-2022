6421197ae2b904e5d69f8cdfc61f3da06821b263
mnbvcxz200 <sam.tas.ti.00@gmail.com>
2022-05-26 19:45:48 +0300

    Updated static root.

--- learningPlatformProject/learningPlatform/settings.py
+++ learningPlatformProject/learningPlatform/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -110,6 +111,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
