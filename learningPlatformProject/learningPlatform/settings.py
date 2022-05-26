56fe082124b7db4280202c1b5e0e7b0d1e09e4fc
mnbvcxz200 <sam.tas.ti.00@gmail.com>
2022-05-26 21:21:38 +0300

    Update settings.

--- learningPlatformProject/learningPlatform/settings.py
+++ learningPlatformProject/learningPlatform/settings.py
@@ -25,8 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'learnApp.apps.LearnappConfig',
-    'learningPlatform',
+    'learnApp',
 ]
 
 MIDDLEWARE = [
