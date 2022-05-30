becb53842171d575a1a1595664eec9ba24d2d8a4
mnbvcxz200 <sam.tas.ti.00@gmail.com>
2022-05-30 17:59:10 +0300

    Minor changes.

--- learningPlatformProject/learningPlatform/settings.py
+++ learningPlatformProject/learningPlatform/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,5 +124,5 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import django_heroku
+
 django_heroku.settings(locals())
